85941d42f7b07092f140a272fae4e1f11e2a81a2
Profi <davlatovtemur97@gmail.com>
2025-12-09 01:46:00 +0600

    Update and rename sw.js to service.js

---
+++ public/service.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'chefzero-v1';
+const STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/animations.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(STATIC_FILES))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
